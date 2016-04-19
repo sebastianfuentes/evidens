a71641d21c62d67014cc419320fc1f05814b501f
Sebastian Fuentes <sebas6297@gmail.com>
2016-04-19 13:11:12 -0500

    :fire: initial commit

---
+++ js/index.js
@@ -0,0 +1,97 @@
+/**
+ * This was built using the scrollie jQuery Plugin
+ * https://github.com/Funsella/jquery-scrollie
+ */
+
+
+$( window ).ready(function() {
+
+    var wHeight = $(window).height();
+
+    $('.slide')
+      .scrollie({
+        scrollOffset : -50,
+        scrollingInView : function(elem) {
+                   
+          var bgColor = elem.data('background');
+          
+          $('body').css('background-color', bgColor);
+          
+        }
+      });
+
+  //----------------------------
+  // Abrir pop-up
+  //----------------------------
+
+  $("#show-pop-up").click(function(event){
+    event.preventDefault();
+    $(".pop-up").show();
+  });
+
+
+  //----------------------------
+  // Abrir infografias
+  //----------------------------
+  $(".cont-infografias img").click(function(){
+    $(".pop-up").show();
+    $(".pop-up img").attr("src", this.src);
+  });
+
+  //----------------------------
+  // Cerrar pop-up al tocar afuera
+  //----------------------------
+
+  $(".pop-up").click(function(event){
+    if($(".pop-up").is(event.target) && $(".pop-up").has(event.target).length ===0)
+    {
+      $(".pop-up").hide();
+    }
+  });
+  //----------------------------
+  // Abrir pop-up
+  //----------------------------
+
+  $("#show-pop-up").click(function(event){
+    event.preventDefault();
+    $(".pop-up").show();
+  });
+
+
+  //----------------------------
+  // Abrir infografias
+  //----------------------------
+  $(".cont-infografias img").click(function(){
+    $(".pop-up").show();
+    $(".pop-up img").attr("src", this.src);
+  });
+
+  //----------------------------
+  // Cerrar pop-up al tocar afuera
+  //----------------------------
+
+  $(".pop-up").click(function(event){
+    if($(".pop-up").is(event.target) && $(".pop-up").has(event.target).length ===0)
+    {
+      $(".pop-up").hide();
+    }
+  });
+
+  $('.modal-trigger').leanModal();
+
+  });
+
+
+//-----inicio lightbox
+$(document).ready(function(){
+  $("a#show-panel").click(function(){
+    $("#lightbox, #lightbox-panel").fadeIn(300);
+  });
+    $("a#close-panel").click(function(){
+    $("#lightbox, #lightbox-panel").fadeOut(300);
+  })
+});
+//-----fin lightbox
+
+
+
---
+++ js/map.js
@@ -0,0 +1,73 @@
+var map;
+var markers = [];
+var image = {
+  url: "https://s3-us-west-1.amazonaws.com/yalku/img/marker.png",
+  scaledSize: new google.maps.Size(24, 32),
+  origin: new google.maps.Point(0, 0),
+  anchor: new google.maps.Point(16, 32)
+};
+var imageMarker = {
+  url: "https://s3-us-west-1.amazonaws.com/yalku/img/placeholder24.png",
+  scaledSize: new google.maps.Size(24, 32),
+  origin: new google.maps.Point(0, 0),
+  anchor: new google.maps.Point(16, 32)
+};
+var lat = 19.3890307
+var lng = -99.1736613;
+var noPoi = [{
+	stylers:[
+		{hue: "#006eff"},
+		{gamma: 0.53},
+		{saturation: -4},
+		{lightness: 29},
+		{invert_lightness: true}
+	]
+},
+{
+  featureType: "poi",
+  elementType: "labels",
+  stylers: [{
+    visibility: "off"
+  }]
+}];
+
+
+function initialize() {
+  var mapOptions = {
+    zoom: 17,
+    center: new google.maps.LatLng(lat, lng),
+    disableDefaultUI: true,
+    scrollwheel: false,
+    styles: noPoi
+  }
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  var location = new google.maps.LatLng(lat, lng);
+  var url = document.getElementById("placeUrl");
+  if (url != null) {
+    url = url.getAttribute("href")
+  }
+  if (url != undefined) {
+    var marker = new google.maps.Marker({
+      position: location,
+      icon: imageMarker,
+      map: map,
+      url: url
+    });
+  } else {
+    var marker = new google.maps.Marker({
+      position: location,
+      icon: imageMarker,
+      map: map
+    });
+  }
+
+  function navigate() {
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+      window.open('geo:' + this.position.lat()+ ',' + this.position.lng()+',15z', '_blank')
+    } else {
+      window.open('https://www.google.com/maps/place//@' + this.position.lat()+ ',' + this.position.lng()+',15z', '_blank')
+    }
+  }
+  google.maps.event.addListener(marker, 'click', navigate);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
