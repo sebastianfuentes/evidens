c596b023881eb6e84cfd5275a5ab26182c17c15b
Sebastian Fuentes <sebas6297@gmail.com>
2016-07-26 22:08:49 -0500

    :fire: ajax get images to load them from the ftp service

--- js/init.js
+++ js/init.js
@@ -1,6 +1,22 @@
 $(function(){
-
+  var row = $('.row.cont-infografias center');
   $('.button-collapse').sideNav();
   $('.parallax').parallax();
-
+  for (var i = 1; i < 13; i++) {
+	  $.ajax({
+	  	url: '/img/infografias/'+i+'.jpg',
+	  	type: 'HEAD'
+	  })
+	  .done(function(e,t,code) {
+	  	if (code.status == 200) {
+	  		row.append("<div class=\"columns small-12 medium-3 image\"><img src=\""+this.url+"\" class=\"responsive-img info\"><\/div>").find('img')
+	  			.unbind('click')
+	  			.click(function(){
+				    $(".pop-up").show();
+				    $(".pop-up img").attr("src", this.src);
+				  });
+	  	}
+	  });
+  }
+  
 }); // end of document ready
